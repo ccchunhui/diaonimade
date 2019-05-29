a900d98922444e371ee5952a4a2db3721f7b6b75
Your Name <506446748@qq.com>
2019-05-29 18:22:59 +0800

    first commit

---
+++ precache-manifest.f8f91851b9d161246abd8b7dec16fd2f.js
@@ -0,0 +1,450 @@
+self.__precacheManifest = [
+  {
+    "revision": "75ee81a27b5da37cb58e058df809de84",
+    "url": "images/gongju.png"
+  },
+  {
+    "revision": "5da36b198739e5dde315bc7a5eef667b",
+    "url": "mui.js"
+  },
+  {
+    "revision": "1e6d535763efa840f9377efc8a9f5ffd",
+    "url": "webviewGroup.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "39091767f6615fc3085954a7559724d8",
+    "url": "images/images/111_02.png"
+  },
+  {
+    "revision": "80820c25839de6dc9cd9",
+    "url": "js/chunk-vendors.552c584f.js"
+  },
+  {
+    "revision": "09d9bbcb88b8c035d6b7669595c0f38c",
+    "url": "images/images/shopbanner1_02.png"
+  },
+  {
+    "revision": "15058133fda60de8117c",
+    "url": "js/about.c6cf919d.js"
+  },
+  {
+    "revision": "1e663262f22dae9bb0b2",
+    "url": "js/app.1a14804b.js"
+  },
+  {
+    "revision": "070dabc6331f1d089f68cace7b947f3c",
+    "url": "images/huoche.png"
+  },
+  {
+    "revision": "327756b1f1ed3200f7dd95404e810db9",
+    "url": "index.html"
+  },
+  {
+    "revision": "032d9de82f5d8198f7084c51bbfd002c",
+    "url": "images/images/shopbanner2_01.png"
+  },
+  {
+    "revision": "9b55d82336774b99f085b4111fdca1c6",
+    "url": "img/headt.9b55d823.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d706277621669531477e6372b7cce7d7",
+    "url": "images/images/shopbanner2_02.png"
+  },
+  {
+    "revision": "9f36a1cdaf6aa28b150c23d95e06ca2b",
+    "url": "images/bjbl.png"
+  },
+  {
+    "revision": "e5e4be6e47f37244ff1b5a1fca8499e3",
+    "url": "images/ybbook.png"
+  },
+  {
+    "revision": "eb932fe3acb176ed7109d7b1bbc57dc1",
+    "url": "images/book2.png"
+  },
+  {
+    "revision": "306e53cc239d8c9cb06cf2d2b9fee600",
+    "url": "images/book.png"
+  },
+  {
+    "revision": "bbaa2a2c0b227a40ce4a97cfc8534ff1",
+    "url": "images/bjbr.png"
+  },
+  {
+    "revision": "69a6920d25bbe97c867781b365b410a4",
+    "url": "images/cap.png"
+  },
+  {
+    "revision": "94bb76e62e3b7654b3c485d34f91fc73",
+    "url": "images/book_2.png"
+  },
+  {
+    "revision": "77c3762d540c1634b9e1fa4393febadb",
+    "url": "images/daizi.png"
+  },
+  {
+    "revision": "04aa076e13a8e20998f66c1023f4a403",
+    "url": "images/date.png"
+  },
+  {
+    "revision": "07033ffd0f82cc47ae6f03bd736ded99",
+    "url": "images/dianzan.png"
+  },
+  {
+    "revision": "69c3a0b78b7cca0c1ecbf13c71dad412",
+    "url": "images/checked.png"
+  },
+  {
+    "revision": "b204aeee52441cbaaa76c53262f8e467",
+    "url": "images/zhexian.png"
+  },
+  {
+    "revision": "2663fac9dc61cf7bd8ee0a94f395b7e1",
+    "url": "images/cap-blue.png"
+  },
+  {
+    "revision": "f3224ace8d97859328f97b447d54d030",
+    "url": "images/dingdan.png"
+  },
+  {
+    "revision": "cb6b572f2098650938e5d33aa0fc9f64",
+    "url": "images/e-book.png"
+  },
+  {
+    "revision": "4ea599a58bc2682687a79296ce891e5d",
+    "url": "images/danci.png"
+  },
+  {
+    "revision": "fb3fffb7fb17f89d08cfd963ee2e393a",
+    "url": "images/f1l.png"
+  },
+  {
+    "revision": "b0daad07379d1bb2b1dff3f075bea93e",
+    "url": "images/f1r.png"
+  },
+  {
+    "revision": "2840e534e601aef52fb3b50d4a8b6810",
+    "url": "images/earphone.png"
+  },
+  {
+    "revision": "a2d2e93131112705541bdf968d4cd37a",
+    "url": "images/en.png"
+  },
+  {
+    "revision": "a5fed2c38876d8bdcf15fe15b9b03779",
+    "url": "images/word.png"
+  },
+  {
+    "revision": "0de4464d8556ed7f605bb60af44d0afe",
+    "url": "images/二维码.png"
+  },
+  {
+    "revision": "c31e2cdb75cb6daa16ed70f4d553058e",
+    "url": "images/write.png"
+  },
+  {
+    "revision": "2a296e6b0a1e2db0361a0c169860fb70",
+    "url": "images/vip.png"
+  },
+  {
+    "revision": "591cd198f1001f84b6473a97e4d22300",
+    "url": "images/xiaoxi.png"
+  },
+  {
+    "revision": "3cad4642553d21c90c2a5a006ae88042",
+    "url": "images/txtpic8.png"
+  },
+  {
+    "revision": "f14d1c98ea1628d157b69308ea47c496",
+    "url": "images/words.png"
+  },
+  {
+    "revision": "29439d0e90d74ba5e8ec17a8ff919cf0",
+    "url": "images/fangxiang.png"
+  },
+  {
+    "revision": "94b8f2d65f38102fcaef4b9349efc722",
+    "url": "images/user.png"
+  },
+  {
+    "revision": "eaba8fa8d406c716569174911cc43032",
+    "url": "images/txtpic5.png"
+  },
+  {
+    "revision": "b2a6ee9e6c10946b9cba273fe0805a54",
+    "url": "images/txtpic7.png"
+  },
+  {
+    "revision": "125abc2a7d6d81c7d93b552b3f51ebc7",
+    "url": "images/find-b2.png"
+  },
+  {
+    "revision": "1d976bc6edfbed53d4d0db1bd129f465",
+    "url": "images/find-b3.png"
+  },
+  {
+    "revision": "03d6d6989532fd2f9744a15a8a2f71f4",
+    "url": "images/find-b4.png"
+  },
+  {
+    "revision": "8e9d23f586bee7ef7eff615275f53883",
+    "url": "images/fukuan.png"
+  },
+  {
+    "revision": "8129170fede8769217b3c93634c573e2",
+    "url": "images/gouxuan1.png"
+  },
+  {
+    "revision": "275e421b2fd4f56912a50c7771a84cd8",
+    "url": "images/gouxuan2.png"
+  },
+  {
+    "revision": "ad4f611124b3ed203ac40e2fa8626028",
+    "url": "images/hot.png"
+  },
+  {
+    "revision": "1da50a7b453e5cb53b77c7e92f0d8b46",
+    "url": "images/geren.png"
+  },
+  {
+    "revision": "1f6717d41388da863cf1b7948ee70504",
+    "url": "images/find-b1.png"
+  },
+  {
+    "revision": "190c9a5c3a4aa914c1a606f5808964c0",
+    "url": "images/images/111_01.png"
+  },
+  {
+    "revision": "f6b2d810c43c0d35c73f663814023991",
+    "url": "images/txtpic3.png"
+  },
+  {
+    "revision": "428a668b4b98c350c0cf7ff821173646",
+    "url": "images/images/111_03.png"
+  },
+  {
+    "revision": "b45a7768365fde33567dbbf1494420f0",
+    "url": "images/txtpic.png"
+  },
+  {
+    "revision": "8e74942b5207620f89824e953570020f",
+    "url": "images/txtpic6.png"
+  },
+  {
+    "revision": "9b55d82336774b99f085b4111fdca1c6",
+    "url": "images/headt.png"
+  },
+  {
+    "revision": "fe538b23c09bfdf24bea80f10dae4a6e",
+    "url": "images/txtpic4.png"
+  },
+  {
+    "revision": "1111ef84fe187d982a9ce0c29febc630",
+    "url": "images/images/shopbanner1_03.gif"
+  },
+  {
+    "revision": "39d82435ea07222038d2964ee2ebc961",
+    "url": "images/txtpic2.png"
+  },
+  {
+    "revision": "84a77493dc336ef67dc232687e8b3487",
+    "url": "images/images/shopbanner2_03.png"
+  },
+  {
+    "revision": "8db8026cdb3a78b7a6384fd09581d6d6",
+    "url": "images/images/shopbanner1_01.gif"
+  },
+  {
+    "revision": "1f0b823d51b6fc138152ef3b1a192188",
+    "url": "images/ios-compass-blue.png"
+  },
+  {
+    "revision": "43670fe269a801990fadc04ac6ef10c5",
+    "url": "images/ios-compass.png"
+  },
+  {
+    "revision": "3d4e119e37ffb5bc51ba43d9946b522c",
+    "url": "images/listen.png"
+  },
+  {
+    "revision": "197628244dddfa400ef9f800a91fe441",
+    "url": "images/lunbo1.png"
+  },
+  {
+    "revision": "dec18f434e682bd5341322692218b2dc",
+    "url": "images/lunbo2.png"
+  },
+  {
+    "revision": "f6cb104193b1e8ddbcc55c81f2ffd4cf",
+    "url": "images/lunbo4.png"
+  },
+  {
+    "revision": "3aa79155a4400099b2a82ffd8e3c8b8b",
+    "url": "images/lunbo5.png"
+  },
+  {
+    "revision": "48e330b8e947fc5449bf8728c98b3fec",
+    "url": "images/jingxuan.png"
+  },
+  {
+    "revision": "dbbf1754bc7c37d81d423d424fa85a8c",
+    "url": "images/microphone.png"
+  },
+  {
+    "revision": "04cbdc3bed9eed7fab9ef272539f1891",
+    "url": "images/method.png"
+  },
+  {
+    "revision": "3e3bd23c36233af1c6b10512122a762e",
+    "url": "images/listpic.png"
+  },
+  {
+    "revision": "cc1292600850957581267b239efe8d01",
+    "url": "images/lunbo3.png"
+  },
+  {
+    "revision": "a726008a812953b80d7a59cc43c4ec2e",
+    "url": "images/more.png"
+  },
+  {
+    "revision": "9b734d418492cbea538c356f4ecbb031",
+    "url": "images/pintu.png"
+  },
+  {
+    "revision": "92c812f014b1ad18b715fb791ad2c0d7",
+    "url": "images/msg.png"
+  },
+  {
+    "revision": "6a2581aeae47fbe68ca0fbdfe2a3c55f",
+    "url": "images/project.png"
+  },
+  {
+    "revision": "b21f22f38280e3fbe10a065ecf3ae162",
+    "url": "images/pencil.png"
+  },
+  {
+    "revision": "1bca19ed9527bbe3b221f14386c7ff8e",
+    "url": "images/search.png"
+  },
+  {
+    "revision": "9f8d4da870f36cabfd1f81075cdd47d8",
+    "url": "images/shouhuo.png"
+  },
+  {
+    "revision": "91e5f90b4984715b5733dff310adea2d",
+    "url": "images/sale1.png"
+  },
+  {
+    "revision": "ca1475af934d8eb884f9511c5b6d8794",
+    "url": "images/python.png"
+  },
+  {
+    "revision": "f5d66548aec66dc7cc6242f43964373e",
+    "url": "images/touxiang.png"
+  },
+  {
+    "revision": "b99feff9a996cdc343c0359381278403",
+    "url": "images/txtpic1.png"
+  },
+  {
+    "revision": "c9ce862adf1fcaf1fed84198db042132",
+    "url": "images/shanbei.png"
+  },
+  {
+    "revision": "1bc653e1416d498fc07f266ef998aebe",
+    "url": "images/siliuji.png"
+  },
+  {
+    "revision": "b3a46b1ebe425939673aaf24873279b0",
+    "url": "images/F2l.png"
+  },
+  {
+    "revision": "f6073d399591aebecb7a097d592393a3",
+    "url": "images/bell.png"
+  },
+  {
+    "revision": "9d8358b3f59a9ec845292bc834ccec6a",
+    "url": "images/X.png"
+  },
+  {
+    "revision": "8adab3061576834fe5b6d9e2e7ebfa42",
+    "url": "images/all.png"
+  },
+  {
+    "revision": "20631d53dfb76456084b4592c890dd9a",
+    "url": "images/banner1.png"
+  },
+  {
+    "revision": "a823f6eb0f7cd536f94c425b08ad0991",
+    "url": "images/banner2.png"
+  },
+  {
+    "revision": "7f39f6f39d0b9922460a11f9fb77119a",
+    "url": "images/F2r.png"
+  },
+  {
+    "revision": "4c0f2ce6f41782cb48e14b6b390b1170",
+    "url": "images/F6l.png"
+  },
+  {
+    "revision": "219b2a732a7616c7eb6bbfba326d0e8b",
+    "url": "images/F6r.png"
+  },
+  {
+    "revision": "da3a557186e9b1f59191fcff8256f7c4",
+    "url": "images/F3r.png"
+  },
+  {
+    "revision": "9a35c5aea4a8df946029549149d36b6b",
+    "url": "images/F4r.png"
+  },
+  {
+    "revision": "c47066341fc820715d944acf2fd908c2",
+    "url": "images/F5r.png"
+  },
+  {
+    "revision": "a0fc43c2736b6c6b8d457ddd838c1904",
+    "url": "images/F5l.png"
+  },
+  {
+    "revision": "bbca915ea62a2310dbe9088282868d75",
+    "url": "images/F4l.png"
+  },
+  {
+    "revision": "1d60004c1f6813ed4853e5576d9dd9dc",
+    "url": "images/F3l.png"
+  },
+  {
+    "revision": "b80e324e0e83db73a4ae06a3d7007aa6",
+    "url": "images/111.png"
+  },
+  {
+    "revision": "017eea3edc245add35c32180fb868fd6",
+    "url": "images/1.png"
+  },
+  {
+    "revision": "8820b7f6582a3c45b7527ae6b183dd2f",
+    "url": "fonts/mui.8820b7f6.ttf"
+  },
+  {
+    "revision": "683f39531666ff9a771a516fecca72f7",
+    "url": "fonts/mui-icons-extra.683f3953.ttf"
+  },
+  {
+    "revision": "80820c25839de6dc9cd9",
+    "url": "css/chunk-vendors.779f7d1d.css"
+  },
+  {
+    "revision": "1e663262f22dae9bb0b2",
+    "url": "css/app.78d54aff.css"
+  }
+];
